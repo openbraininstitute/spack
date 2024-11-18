a9b8f1e999d09c49ab7fd62b84b11f48cf578b9c
Matthias Wolf <matthias.wolf@epfl.ch>
2024-11-18 16:09:42 +0100

    nmodl: pull in submodules.
    Should allow for a full build with stock Spack, where .git is _not_
    preserved.  We only need the coding conventions, though.  Adding them as
    a resource will be too complex.

--- bluebrain/repo-bluebrain/packages/nmodl/package.py
+++ bluebrain/repo-bluebrain/packages/nmodl/package.py
@@ -12,6 +12,8 @@ class Nmodl(CMakePackage):
     url = "https://github.com/BlueBrain/nmodl.git"
     git = "https://github.com/BlueBrain/nmodl.git"
 
+    submodules = True
+
     version("develop", branch="master", submodules=True)
     version("llvm", branch="llvm", submodules=True)
     version("0.7.a4", commit="79101467912f9690b6c40a1d3eed7315e3ea9bc4")
@@ -46,8 +48,8 @@ class Nmodl(CMakePackage):
     depends_on("fmt+pic", when="+python")
     depends_on("fmt@8.0:8", when="@:0.4 ~python")
     depends_on("fmt+pic@8.0:8", when="@:0.4 +python")
-    depends_on("fmt@8:", when="@0.5: ~python")
-    depends_on("fmt+pic@8:", when="@0.5: +python")
+    depends_on("fmt@8:10", when="@0.5: ~python")
+    depends_on("fmt+pic@8:10", when="@0.5: +python")
     depends_on("nlohmann-json")
     depends_on("python@3.6.0:")
     depends_on("py-jinja2@2.10:", type=("build", "run"))
