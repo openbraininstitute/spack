fbb3121218e2a382327117c5f1fdb62ab0a22dc8
Alessandro Cattabiani <cattabiani@users.noreply.github.com>
2025-06-16 13:26:40 +0200

    py-libsonata: add develop (#16)
    * py-libsonata: add develop version. Change maintaners
    
    * add maintainers

--- var/spack/repos/builtin/packages/py-libsonata/package.py
+++ var/spack/repos/builtin/packages/py-libsonata/package.py
@@ -13,8 +13,9 @@ class PyLibsonata(PythonPackage):
     git = "https://github.com/openbraininstitute/libsonata.git"
     pypi = "libsonata/libsonata-0.1.14.tar.gz"
 
-    maintainers("tristan0x")
+    maintainers("cattabiani", "mikeg")
 
+    version("develop", branch="master")
     version("master", branch="master")
     version("0.1.28", sha256="9421366a2b2cd5b3c0d0f62a5aaea852949e60bac3032a3161bf0bbb107dada9")
     version("0.1.26", sha256="b653cbefbc511fe24ccf5cce7d80253954ec280800af5fd33700f0faea93fd4c")
