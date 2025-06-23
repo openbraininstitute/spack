2a435d6b458f44f9796bd215d56db6abf554d49b
Alessandro Cattabiani <cattabiani@users.noreply.github.com>
2025-06-23 11:19:37 +0200

    meson, neuron: fixes for neurodamus_models (#17)
    * meson: 1.2.2 had a typo in the linker search for clang. 1.4 has this fixed. Bump version
    neuron: FENV_ACCESS ON is not allowed when math is not precise. -ffinite-math-only is
    	enough for gcc to make it ON without complaining. For clang we need to specify
            at least ffp: precise to the compiler. However, doing this from spack is not
            possible because when we set -ffinite-math-only in neuron we override. As a
            workaround we set NRN_ENABLE_MATH_OPT=OFF for now.
    
    * add link

--- bluebrain/repo-patches/packages/meson/package.py
+++ bluebrain/repo-patches/packages/meson/package.py
@@ -5,4 +5,6 @@ from spack.pkg.builtin.meson import Meson as BuiltinMeson
 class Meson(BuiltinMeson):
     __doc__ = BuiltinMeson.__doc__
 
+    version("1.4.0", sha256="61382f295378bddcd9bebb3a9a9065b1cbc671fa41b80964ab02726f9a5f3a88")
+
     patch("pgmath.patch", when="target=aarch64:")
