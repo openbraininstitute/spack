1e360652d76d41783cc013dd90d27b2d9b1a23a4
Alessandro Cattabiani <cattabiani@users.noreply.github.com>
2025-10-13 11:10:37 +0200

    libsonata-report: bump 1.2.6 (#26)
    * libsonata-report: bump 1.2.6

--- bluebrain/repo-bluebrain/packages/libsonata-report/package.py
+++ bluebrain/repo-bluebrain/packages/libsonata-report/package.py
@@ -18,6 +18,7 @@ class LibsonataReport(CMakePackage):
     git = "https://github.com/openbraininstitute/libsonatareport.git"
 
     version("develop", branch="master", submodules=False, get_full_repo=True)
+    version("1.2.6", tag="1.2.6", submodules=False)
     version("1.2.5", tag="1.2.5", submodules=False)
     version("1.2.4", tag="1.2.4", submodules=False)
     version("1.2.3", tag="1.2.3", submodules=False)
