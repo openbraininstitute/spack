2f2b6934eefab81716e95489405cd04e96a1a943
Alessandro Cattabiani <alessandro.cattabiani@gmail.com>
2025-10-14 12:34:07 +0200

    py-libsonata: bump 0.1.31

--- var/spack/repos/builtin/packages/py-libsonata/package.py
+++ var/spack/repos/builtin/packages/py-libsonata/package.py
@@ -17,6 +17,7 @@ class PyLibsonata(PythonPackage):
 
     version("develop", branch="master")
     version("master", branch="master")
+    version("0.1.31", sha256="4de060b4c613f706a28cb0ef267b8a05dfbe3cdc7d1f5e3694c0a1ba103e19cf")
     version("0.1.30", sha256="964c50235456f8e1d2e75b0c35e9a19d645046f701f28cbcde33bf0b9c9e0084")
     version("0.1.28", sha256="9421366a2b2cd5b3c0d0f62a5aaea852949e60bac3032a3161bf0bbb107dada9")
     version("0.1.26", sha256="b653cbefbc511fe24ccf5cce7d80253954ec280800af5fd33700f0faea93fd4c")
